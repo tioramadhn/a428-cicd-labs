04c2d5cc050837c834708828a6a63c6824c0828f
tioramadhn <tioramadhntio@aapps.ipb.ac.id>
2023-06-27 16:23:43 +0700

    add sleep 1m

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-
+echo 'Jeda 1 Menit sebelum aplikasi ditutup'
+sleep 1m
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
